62fc80deca07dd0e3dfb727989af7e0a1168cb73
Tommy Guy <richardtguy84@gmail.com>
2017-05-22 19:56:00 -0700

    updates and comments

--- 2017/Com597I/Week7/solution.py
+++ 2017/Com597I/Week7/solution.py
@@ -1,4 +1,4 @@
-# clean the congress data for further analysis
+# clean the congress data
 
 # column mappings:
 #  construction: 37
@@ -12,25 +12,32 @@ file_handle.readline()
 
 output_file = open("cd1141dp3_clean.csv", "w")
 
+# output a header
 output_file.write("total,constr,manuf,finance,constr_r,manuf_r,finance_r,mean_income,median_income")
 output_file.write("\n")
 
 for line in file_handle:
-    line_parts = line.split(',')  # 
+    line_parts = line.split(',')  # split the line into parts based on ','
     
+    # eliminate any lines where the second column is PR or DC. Remember that continue means 
+    # "skip" this iteration.
     if line_parts[1] == 'PR' or line_parts[1] == "DC":
         continue
-        
+
+    # assign each of the variables we want to keep.
     total_workforce = float(line_parts[5])
     construction = float(line_parts[36])
     manufactoring = float(line_parts[37])
     finance = float(line_parts[42])
     median_income = float(line_parts[64])
     mean_income = float(line_parts[65])
-    
+
+    # compute additional columns as ratios
     construction_ratio = construction / total_workforce
     manufactoring_ratio = manufactoring / total_workforce
     finance_ratio = finance / total_workforce
+
+    # create a list of all of the variables to output
     output_list = [str(total_workforce), str(construction), 
             str(manufactoring), str(finance), 
             str(construction_ratio), str(manufactoring_ratio), 
